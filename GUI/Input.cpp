589533840ead4f6514f5ef258a7959d38665ba17
Hassan Saied <hsaied1787@hotmail.com>
2016-05-18 16:25:56 +0200

    Need to test

--- GUI/Input.cpp
+++ GUI/Input.cpp
@@ -1,5 +1,4 @@
 #include "Output.h"
-#include"..\Components\Gate.h"
 Graph *Input::Interface = new Graph;
 Input::Input(window* pW)
 {
@@ -37,7 +36,11 @@ pair< ActionType, vector<GridItem*> > Input::GetUserAction() const
 	tmp = Interface->getAction(GraphicsInfo(x, y));
 	if (tmp == NULL)
 		return make_pair(DSN_AREA,ReturnedPointers);
-	else if (dynamic_cast<Gate*>(tmp));
+	else 
+	{
+		ReturnedPointers.push_back(tmp);
+		return make_pair(tmp->Leftpress(NULL), ReturnedPointers);
+	}
 
 }
 
